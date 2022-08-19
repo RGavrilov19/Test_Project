086bb95e4866395ed479d1a8d48701a90e2443d3
Roman <rgavrilov@ibs.ru>
2022-08-19 15:26:01 +0300

    Задача №12

--- src/com/max/idea/Task_12.java
+++ src/com/max/idea/Task_12.java
@@ -1,9 +1,13 @@
 package com.max.idea;
 
 
+import java.util.Scanner;
+
 public class Task_12 {
     public static void main(String[] args) {
-        String x = "I like Java!!!";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String x = in.nextLine();
         if(x.contains("Java") && x.startsWith("I like") && x.endsWith("!!!")) {
             System.out.println(x.toUpperCase());
         }
