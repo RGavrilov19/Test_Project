48694da0f5a4bea18fb8eccec4b78d38ed64138e
Roman <rgavrilov@ibs.ru>
2022-08-19 15:24:18 +0300

    Задача №12

---
+++ src/com/max/idea/Task_12.java
@@ -0,0 +1,12 @@
+package com.max.idea;
+
+
+public class Task_12 {
+    public static void main(String[] args) {
+        String x = "I like Java!!!";
+        if(x.contains("Java") && x.startsWith("I like") && x.endsWith("!!!")) {
+            System.out.println(x.toUpperCase());
+        }
+        System.out.println((x.substring(7,11)).replace('a', 'o'));
+    }
+}
